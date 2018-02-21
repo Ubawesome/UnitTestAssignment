896a1d8cc3da574e729fb4ccf0dff4c1f1cd5d83
Jakob Miller <JakobM569@gmail.com>
2018-02-21 15:51:09 -0800

    aDDED bank tests

--- UnitTestAssignment/ClassLibrary/BankAccount.cs
+++ UnitTestAssignment/ClassLibrary/BankAccount.cs
@@ -12,29 +12,42 @@ namespace BankLib
 
         public BankAccount()
         {
-
+            balance = 0;
         }
 
         public BankAccount(int startBal)
         {
-
+            balance = startBal;
         }
 
         public int getBalance()
         {
-            return 0;
+            return balance;
         }
 
         // Withdraws money from account.  
         // Return true if withdrawl successful, false if amt is negative or would overdraw account (do not perform transaction in those cases)
         public bool withdraw(int amt)
         {
+            if (amt > balance)
+            { return false; }
+            
+            if (amt <= 0)
+            { return false; }
+
+            balance -= amt;
+
             return true;
         }
 
         // Deposits amt into acocunt.  Returns true is operation successful, false otherwise.
         public bool deposit(int amt)
         {
+            if (amt <= 0)
+            { return false;  }
+
+            balance += amt;
+
             return true;
         }
     }
---
+++ UnitTestAssignment/UnitTestAssignment/BankTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankLib;
+
+namespace UnitTestAssignment
+{
+
+    [TestClass]
+    public class BankTest
+    {
+        [TestMethod]
+        public void testDefaultCon()
+        {
+            BankAccount account = new BankAccount();
+
+            Assert.AreEqual(0, account.getBalance());
+        }
+
+        [TestMethod]
+        public void testConstructor()
+        {
+            BankAccount account = new BankAccount(500);
+
+            Assert.AreEqual(500, account.getBalance());
+        }
+
+        [TestMethod]
+        public void testDeposit()
+        {
+            BankAccount account = new BankAccount();
+
+            Assert.AreEqual(true, account.deposit(500));
+
+            Assert.AreEqual(500, account.getBalance());
+
+            Assert.AreEqual(false, account.deposit(-500));
+
+            Assert.AreEqual(false, account.deposit(0));
+
+            Assert.AreEqual(500, account.getBalance());
+        }
+
+        [TestMethod]
+        public void testWithdraw()
+        {
+            BankAccount account = new BankAccount(500);
+
+            Assert.AreEqual(true, account.withdraw(500));
+
+            Assert.AreEqual(0, account.getBalance());
+
+            Assert.AreEqual(false, account.withdraw(500));
+
+            Assert.AreEqual(false, account.withdraw(-500));
+
+            Assert.AreEqual(false, account.withdraw(0));
+
+            Assert.AreEqual(0, account.getBalance());
+
+        }
+    }
+}
